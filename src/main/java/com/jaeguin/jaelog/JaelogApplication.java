541eade7e86e0d4e8942475b16391d1e2585871f
penguin <gagle7664@gmail.com>
2022-06-08 10:57:56 +0900

    start

---
+++ src/main/java/com/jaeguin/jaelog/JaelogApplication.java
@@ -0,0 +1,13 @@
+package com.jaeguin.jaelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaelogApplication.class, args);
+	}
+
+}
