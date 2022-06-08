faeee5b2006f89a27d6fec680fd9d0cae598ddc6
penguin <gagle7664@gmail.com>
2022-06-08 22:46:04 +0900

    password 암호화 구현

--- src/main/java/com/jaeguin/jaelog/web/user/service/UserService.java
+++ src/main/java/com/jaeguin/jaelog/web/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.jaeguin.jaelog.web.user.service;
 import com.jaeguin.jaelog.domain.user.entity.User;
 import com.jaeguin.jaelog.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +12,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
     public Long register(User user) {
+        String password = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(password);
         return userRepository.save(user).getId();
     }
+
 }
