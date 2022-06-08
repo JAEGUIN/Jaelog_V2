61db225914e52a9a0ff68a82cce5a898f13441ad
penguin <gagle7664@gmail.com>
2022-06-08 12:00:25 +0900

    기본 로그인,회원가입 페이지 구현

---
+++ src/main/java/com/jaeguin/jaelog/web/user/UserController.java
@@ -0,0 +1,18 @@
+package com.jaeguin.jaelog.web.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/user/login")
+    public String login(){
+        return "user/loginForm";
+    }
+
+    @GetMapping("/user/register")
+    public String register(){
+        return "user/registerForm";
+    }
+}
