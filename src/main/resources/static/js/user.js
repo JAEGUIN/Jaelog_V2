aa6082089f05a68e472a1f894b2f733600dc45d1
penguin <gagle7664@gmail.com>
2022-06-09 15:39:08 +0900

    비밀번호 확인 구현

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,12 +1,30 @@
 'use strict';
 
+var compare_result = false;
+function fn_compare_pw(){
+        var pw = $("#password").val();
+        var pw2 = $("#password2").val();
+
+        if(pw == pw2){
+            compare_result = true;
+            $('#s_result').text('비밀번호가 일치합니다.').css({color:"green"});
+        }else{
+            compare_result = false;
+            $('#s_result').text('비밀번호가 일치하지 않습니다 !!').css({color:"red"});
+        }
+    }
+
 let index = {
     init: function () {
         $("#btn-save").on("click", () => { //this를 바인딩하기 위해 화샬표 함수 사용
             let form = document.querySelector("#needs-validation");
+            var pw = $("#password").val();
+            var pw2 = $("#password2").val();
             if (form.checkValidity() == false) {
                 console.log("회원가입 안됨")
-            } else {
+            } if (pw != pw2) {
+                console.log("회원가입 안됨")
+            }else {
                 this.save();
             }
         });
@@ -30,7 +48,7 @@ let index = {
             alert("회원가입이 완료되었습니다.");
             location.href = "/";
         }).fail(function (err) {
-            alert(JSON.stringify(err));
+            alert('이미 가입된 회원입니다.');
         });
     }
 }
